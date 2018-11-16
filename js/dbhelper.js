b8e7268d92edf6c705945620d4e3fa7050bdf662
Ibrahim Kundlik <36952328+ibrahimkundlik@users.noreply.github.com>
2018-11-16 12:16:39 +0530

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -151,7 +151,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`);
   }
 
   /**
@@ -167,16 +167,5 @@ class DBHelper {
       marker.addTo(newMap);
     return marker;
   } 
-  /* static mapMarkerForRestaurant(restaurant, map) {
-    const marker = new google.maps.Marker({
-      position: restaurant.latlng,
-      title: restaurant.name,
-      url: DBHelper.urlForRestaurant(restaurant),
-      map: map,
-      animation: google.maps.Animation.DROP}
-    );
-    return marker;
-  } */
-
 }
 
